f41b4a68c545d9ecd6981414e68f1197f6286774
Allyson English <58833140+Allyson-English@users.noreply.github.com>
2020-08-01 17:33:28 -0400

    updating play screen

--- src/StartupScene.js
+++ src/StartupScene.js
@@ -46,8 +46,8 @@ StartupScene.prototype._drawControlsHelp = function (ctx) {
   ctx.fillStyle = "#d0e4f7";
   ctx.font = "bold 14px 'Lucida Console', Monaco, monospace"
   ctx.fillText("CONTROL: ARROW KEYS", 187, 300);
-  ctx.fillText("Help Dr. Fauci collect virus samples for testing while", 40, 335);
-  ctx.fillText("avoiding the anti-maskers. Pick up hand sanitizer to", 45, 355);
-  ctx.fillText("give virus deniers a mask and send them back to home base.", 36, 370);
-  ctx.fillText("And collect test tubes for extra points.", 110, 385);
+  ctx.fillText("Help Dr. Fauci keep the American public healthy", 80, 335);
+  ctx.fillText("in this pac-man inspired game. To keep the fun going, ", 50, 355);
+  ctx.fillText("challenge your friends (over zoom)", 130, 375);
+  ctx.fillText("and wear a mask in public!", 160, 395);
 };
